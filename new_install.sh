b67895b440e955d70f7469116d2c464873a59424
Samuel Kunst <samuel.kunst@gmail.com>
2020-07-21 09:34:28 +0200

    add fresh installation script

---
+++ new_install.sh
@@ -0,0 +1,9 @@
+echo "* Running configuration setup..."
+./setup.sh
+echo "* Updating homebrew..."
+brew update
+echo "* Installing Google Chrome..."
+brew install google-chrome
+echo "* Installing iTerm2..."
+brew install iterm2
+
