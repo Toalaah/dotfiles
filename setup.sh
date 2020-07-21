6753b361f74b31cc7dc3f16f7573600ddd0d5c47
Samuel Kunst <samuel.kunst@gmail.com>
2020-07-21 09:21:18 +0200

    remove placeholders in setup script

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@ echo "* searching for pre-existing config files...\n"
 
 # check for existing files in home dir, make backup
 echo "* creating backups...\n"
-mv $HOME/.zshrc_ $HOME/.zshrc.bak &> ./log
-mv $HOME/.vimrc_ $HOME/.vimrc.bak &> ./log
-mv $HOME/.tmux.conf_ $HOME/.tmux.conf.bak &> ./log
+mv $HOME/.zshrc $HOME/.zshrc.bak &> ./log
+mv $HOME/.vimrc $HOME/.vimrc.bak &> ./log
+mv $HOME/.tmux.conf $HOME/.tmux.conf.bak &> ./log
 rm ./log
 
 echo "* creating new source files...\n"
 
 # create new dotfiles through reference
-echo "source $HOME/config/zsh/.zshrc" >$HOME/test_zsh.txt
-echo "source $HOME/config/vim/.vimrc" >$HOME/test_vim.txt
-echo "source $HOME/config/tmux/.tmux.conf" >$HOME/test_tmux.txt
+echo "source $HOME/config/zsh/.zshrc" >$HOME/.zshrc
+echo "source $HOME/config/vim/.vimrc" >$HOME/.vimrc
+echo "source $HOME/config/tmux/.tmux.conf" >$HOME/.tmux.conf
 
 echo "* done"
