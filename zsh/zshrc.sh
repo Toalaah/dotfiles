8ec2fceb0486a6f929f516dc19d6671c6dac874e
Samuel Kunst <samuel.kunst@gmail.com>
2020-08-07 14:12:06 +0200

    fix bug causing extra newline when using multiple branches

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -16,11 +16,10 @@ function update_prompt() {
     PS1+="%F{109}"
 
     # find out if in git repo or not, and if so what branch
-    BRANCH=$(git branch 2>~/tmp/null |awk '{print $2}')
+    BRANCH=$(git symbolic-ref --short HEAD 2> /dev/null)
     if [ -z "$BRANCH" ]; then
         PS1+=""
     else
-        # alternate version which shows changes instead of commits
         CHANGES=$(git s --short | wc -l | awk '{$1=$1};1')
         if [ $CHANGES -eq 0 ]; then
             PS1+=", ($BRANCH)"
@@ -43,4 +42,4 @@ pdflatex --interaction=batchmode "$1" && latexmk -c "$1"
 }
 source $HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 update_prompt
-chpwd() ls;  # auto-cd
+chpwd() ls;  # auto-ls
