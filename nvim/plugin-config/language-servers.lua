579b8c7648aa43c473a3c1ce72476030971da73b
Samuel Kunst <samuel.kunst@gmail.com>
2021-05-22 11:10:27 +0200

    fix lsp bug with latex documents

--- nvim/plugin-config/language-servers.lua
+++ nvim/plugin-config/language-servers.lua
@@ -2,5 +2,7 @@ require'lspconfig'.clangd.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.vimls.setup{}
 require'lspconfig'.tsserver.setup{}
-require'lspconfig'.texlab.setup{}
+-- Sometimes the tex-lsp is not found unless the 
+-- appropriate filetypes are explicitly provided
+require'lspconfig'.texlab.setup{ filetypes = { ".tex", ".bib" } }
 require'lspconfig'.bashls.setup{}
