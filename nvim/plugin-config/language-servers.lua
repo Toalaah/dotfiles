736a33708aa083259cbb2a6b521c7444b3b9b747
Samuel Kunst <samuel.kunst@gmail.com>
2021-05-23 11:03:11 +0200

    Improve tex-lsp detection

--- nvim/plugin-config/language-servers.lua
+++ nvim/plugin-config/language-servers.lua
@@ -2,7 +2,5 @@ require'lspconfig'.clangd.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.vimls.setup{}
 require'lspconfig'.tsserver.setup{}
--- Sometimes the tex-lsp is not found unless the 
--- appropriate filetypes are explicitly provided
-require'lspconfig'.texlab.setup{ filetypes = { ".tex", ".bib" } }
+require'lspconfig'.texlab.setup{}
 require'lspconfig'.bashls.setup{}
