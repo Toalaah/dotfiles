d0c692e53878c89aef428606e7f2ac9bd5a68733
Samuel Kunst <samuel.kunst@gmail.com>
2021-08-03 09:25:43 +0200

    change status-/bufferline plugins + configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,13 @@
+-- plugin sourcing
 require("plugin-config/plugins")
+
+
+-- plugins / lsp configuraitions
 require("plugin-config/language-servers")
-require("plugin-config/lightline-config")
+require("plugin-config/lualine-config")
+require("plugin-config/bufferline-config")
+
+
+-- general preferences, bindings, etc
 require("preferences")
 require("keybindings")
--- nvim/.config/nvim/lua/plugin-config/plugins.lua
+++ nvim/.config/nvim/lua/plugin-config/plugins.lua
@@ -1,24 +1,23 @@
- --[[
-  __   _(_)_ __ ___  _ __ ___
-  \ \ / / | '_ ` _ \| '__/ __|
-   \ V /| | | | | | | | | (__   v3
-    \_/ |_|_| |_| |_|_|  \___|  07/21
---]]
-
-
 return require('packer').startup(function()
 	use {'wbthomason/packer.nvim' }
 	use { 'gruvbox-community/gruvbox' }
-        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use { 'neovim/nvim-lspconfig' }
 	use { 'tpope/vim-fugitive' }
-	use { 'itchyny/lightline.vim' }
-	use { 'mengelbrecht/lightline-bufferline' }
+
+
+  use { 'hoob3rt/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons' } }
+  use { 'akinsho/nvim-bufferline.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
+
+  -- status / tab bar stuff
+	--[[ use { 'itchyny/lightline.vim' }
+	use { 'mengelbrecht/lightline-bufferline' } ]]
+
 	use { 'jiangmiao/auto-pairs' }
 	use { 'b3nj5m1n/kommentary' }
 	use { 'iamcco/markdown-preview.nvim' }
 
-	-- Telescope stuff
+	-- telescope stuff
 	use { 'nvim-lua/popup.nvim' }
 	use { 'nvim-lua/plenary.nvim' }              
 	use { 'nvim-telescope/telescope.nvim' }
