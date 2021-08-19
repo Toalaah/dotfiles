605b887692d15b3e2108e37ded9613ac6ee332c7
Samuel Kunst <samuel.kunst@gmail.com>
2021-08-19 17:06:09 +0200

    Update installer

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-function test_check_dependencies() {
-  for PROG in 'brew' 'git' 'node' 'stow'
-  do
+function check_dependencies() {
+  for PROG in 'mailhog' 'brew' 'git' 'node' 'stow'; do
     command -v $PROG &> /dev/null || print_dependency_error_and_exit
   done
 
 }
 
 function print_dependency_error_and_exit() {
-  echo 'Missing dependencies!'
+  printf 'Missing dependencies! Required dependencies:\n\n'
+  for PROG in 'brew' 'git' 'node' 'stow'; do
+    printf $PROG'\n'
+  done
+  printf '\nPlease make sure these are all installed and in your path before proceeding!\n'
   exit 1
 }
 
 function symlink_dotfiles() {
-  for PROG in 'alacritty' 'nvim' 'tmux' 'zsh'
-  do
+  for PROG in 'alacritty' 'nvim' 'tmux' 'zsh'; do 
     stow $PROG 
   done
 }
 
-
 # Check dependencies + install if necessary (This includes the font for alacritty!)
+check_dependencies
 # Stow files
 # Install language servers
 # Creating an ssh key just for you... ( + show link )
-
-
-test_check_dependencies
-echo 'more scripts'
 exit 0
+
