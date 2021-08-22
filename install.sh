cb650fa18a1e00d4d9319203e254d1dfef511401
Samuel Kunst <samuel.kunst@gmail.com>
2021-08-22 12:49:42 +0200

    improve installer

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 dependencies=()
 dotfiles=("alacritty" "nvim" "tmux" "zsh")
 RED="\033[0;31m"
 CYAN="\033[0;36m"
 NC="\033[0m"
+REPO="toalaah/config"
+HOME=$HOME
+DEST="${HOME}/.dotfiles"
 
 function determine_os_type {
   [[ $OSTYPE == "linux-gnu"* ]] && dependencies=("wget" "git" "npm" "stow") && return
@@ -28,18 +31,20 @@ function print_dependency_error_and_exit {
 }
 
 function symlink_dotfiles {
+  # we need to delete any existing files to avoid stow conflicts...
   for PROG in ${dotfiles[@]}; do 
-    stow -S $PROG 
+    rm -rf $(find "${DEST}/${PROG}" -type f | sed "s/\/.dotfiles\/${PROG}//g") ||:
+    stow -d "${DEST}" -S "${PROG}" 
   done
 }
 
 function install_font {
   # all fonts: https://www.nerdfonts.com/font-downloads
   FONT_NAME=$1
-  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$FONT_NAME.zip -O /tmp/font.zip
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$FONT_NAME.zip -O /tmp/font.zip > /dev/null 2>&1
   unzip -n /tmp/font.zip -d ~/.local/share/fonts > /dev/null 2>&1
   fc-cache -fv > /dev/null 2>&1
-  rm $FONT_NAME.zip
+  rm /tmp/font.zip
 }
 
 function print_status {
@@ -50,7 +55,7 @@ function ask_for_overwrite {
   printf "[${RED}Warning! The following files will be created / overwritten!${NC}]\n"
   for PROG in ${dotfiles[@]}; do 
     # black magic replacing to get all files which will be overwritten
-    printf "$(find $PROG -type f | sed 's/^[^\/]*\//~\//g')\n"
+    printf "$(find "${DEST}/${PROG}" -type f | sed 's/^[^\/]*\//~\//g')\n"
   done
   while true; do
       read -p "Do you wish to continue? [y/N] " yn
@@ -71,12 +76,16 @@ function main {
   determine_os_type
   print_status "Checking dependencies..."
   check_dependencies
-  print_status "Done"
   print_status "Requirements met"
+  print_status "Cloning repo to ${DEST}"
+
+  mkdir -p "${HOME}/$DEST"
+  git clone "https://github.com/${REPO}" "$DEST"
+
   ask_for_overwrite
   print_status "Installing dotfiles..."
   # leave this off for debugging 
-  # symlink_dotfiles
+  symlink_dotfiles
   print_status "Done"
   print_status "Installing fonts..."
   install_font "UbuntuMono"
