8a93330a9abdaf61d8a44c141286739c9ca3aada
Samuel Kunst <samuel.kunst@verimi.com>
2021-09-17 10:43:06 +0200

    add packer bootstrapping and update lsp-config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,7 +3,6 @@ require('plugin-config.plugins')
 
 -- plugin / lsp configurations
 require('plugin-config.lsp-install')
-require('plugin-config.lsp-config')
 require('plugin-config.lualine-config')
 require('plugin-config.bufferline-config')
 require('plugin-config.compe-config')
