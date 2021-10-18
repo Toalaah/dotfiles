68a2c0cebcfc7a546187b10d99d68088c31f9250
Samuel Kunst <samuel.kunst@verimi.com>
2021-10-18 10:47:22 +0200

    nvim: update nvim-tree config

--- nvim/.config/nvim/lua/plugin-config/nvim-tree-config.lua
+++ nvim/.config/nvim/lua/plugin-config/nvim-tree-config.lua
@@ -1,35 +1,36 @@
-vim.g.nvim_tree_side = 'left'
-vim.g.nvim_tree_auto_close =1
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_width = 20
-vim.g.nvim_tree_icons = {
-     default = '',
-     symlink = '',
-     git = {
-       unstaged = "✗",
-       staged = "✓",
-       unmerged = "",
-       renamed = "➜",
-       untracked = "★",
-       deleted = "",
-       ignored = "◌"
-       },
-     folder = {
-       arrow_open = "",
-       arrow_closed = "",
-       default = "",
-       open = "",
-       empty = "",
-       empty_open = "",
-       symlink = "",
-       symlink_open = "",
-       },
-       lsp = {
-         hint = "",
-         info = "",
-         warning = "",
-         error = "",
+require'nvim-tree'.setup {
+  tree_side = 'left',
+  tree_auto_close =1,
+  tree_auto_open = 1,
+  tree_follow = 1,
+  tree_width = 20,
+  tree_icons = {
+       default = '',
+       symlink = '',
+       git = {
+         unstaged = "✗",
+         staged = "✓",
+         unmerged = "",
+         renamed = "➜",
+         untracked = "★",
+         deleted = "",
+         ignored = "◌"
+         },
+       folder = {
+         arrow_open = "",
+         arrow_closed = "",
+         default = "",
+         open = "",
+         empty = "",
+         empty_open = "",
+         symlink = "",
+         symlink_open = "",
+         },
+         lsp = {
+           hint = "",
+           info = "",
+           warning = "",
+           error = "",
+         }
        }
-     }
-
+  }
--- nvim/.config/nvim/lua/preferences.lua
+++ nvim/.config/nvim/lua/preferences.lua
@@ -28,7 +28,7 @@ vim.cmd('autocmd BufRead,BufNew,BufEnter * highlight Normal guibg=none')
 -- disables auto-comment continuations for all files.
 -- Some files have their format-options overwritten by
 -- ftplugins; this ensures that they are set correctly
-vim.cmd('autocmd BufRead,BufNew,BufEnter *.* set formatoptions-=cro')
+vim.cmd('autocmd BufRead,BufNew,BufEnter *.py* set formatoptions-=cro shiftwidth=4 tabstop=4 expandtab')
 -- run xrdb on changes to xresources
 vim.cmd('autocmd BufWritePost ~/.xresources !xrdb -DPYWAL_="<$HOME/.cache/wal/colors.Xresources>" -merge ~/.xresources')
 -- recompile dwm config on write
