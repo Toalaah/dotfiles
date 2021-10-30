0bcc0d2889af94b971fdbd4e9a967b70f133e30d
Samuel Kunst <samuel.kunst@gmail.com>
2021-10-30 08:59:55 +0200

    nvim: swap nvim-compe plugin -> nvim-cmp

---
+++ nvim/.config/nvim/lua/plugin-config/cmp-config.lua
@@ -0,0 +1,39 @@
+local cmp = require'cmp'
+
+  cmp.setup({
+    mapping = {
+      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable,
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'buffer' },
+    })
+  })
+
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+  -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  -- require('lspconfig')[%YOUR_LSP_SERVER%].setup {
+    -- capabilities = capabilities
+  -- }
--- nvim/.config/nvim/lua/plugin-config/plugins.lua
+++ nvim/.config/nvim/lua/plugin-config/plugins.lua
@@ -19,8 +19,12 @@ return require('packer').startup(function(use)
   use { 'apzelos/blamer.nvim' }
 
   -- lsp / auto-completion
-  use { 'hrsh7th/nvim-compe' }
   use { 'neovim/nvim-lspconfig' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-cmdline' }
+  use { 'hrsh7th/nvim-cmp' }
   use { 'kabouzeid/nvim-lspinstall' }
 
   -- flutter / dart development
