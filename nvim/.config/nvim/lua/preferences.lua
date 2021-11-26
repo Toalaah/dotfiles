30ed90b564674977c7964920da9ec63c2ec4356f
Samuel Kunst <samuel.kunst@gmail.com>
2021-11-26 09:40:04 +0100

    nvim: refactor preferences file

--- nvim/.config/nvim/lua/preferences.lua
+++ nvim/.config/nvim/lua/preferences.lua
@@ -1,49 +1,58 @@
-vim.cmd('set encoding=utf-8')         -- set default encoding standard
-vim.cmd('set hidden')                 -- allows switching buffers without saving them
-vim.cmd('set shortmess+=c')           -- disable 'pattern not found' message from compe
-vim.cmd('set relativenumber')         -- relative line numbering, better for jumps
-vim.cmd('set termguicolors')          -- enable 24-bit rgb
-vim.cmd('set tabstop=2')              -- sets tab-length to 2 instead of the default 8
-vim.cmd('set shiftwidth=2')           -- enables auto-indenting to the same degree of tab-lengths
-vim.cmd('set expandtab')              -- converts all tabs to spaces on save
-vim.cmd('set nu')                     -- show numbers on side
-vim.cmd('set noswapfile')             -- disable swap-files
-vim.cmd('set nobackup')               -- disable backup files
-vim.cmd('set incsearch')              -- incremental searching
-vim.cmd('set ignorecase')             -- searching ignores case
-vim.cmd('set cursorline')             -- highlights current line
-vim.cmd('set scrolloff=8')            -- always show 8 lines on the top / bottom of the buffer when scrolling
-vim.cmd('set nowrap')                 -- disable linewrapping
-vim.cmd('set noshowmode')             -- disables messages below the tabline
-vim.cmd('set clipboard+=unnamedplus') -- enables copy / pasting to and from the buffer
-vim.cmd('set showtabline=2')          -- always show tab-bar
-vim.cmd('set mouse=a')                -- enable mouse usage
-vim.cmd('set autoindent')             -- auto-indenting
-vim.cmd('colorscheme gruvbox')        -- set colorscheme
-vim.cmd('set bg=dark')                -- set background-color
-vim.cmd('let g:blamer_enabled=1')     -- enable 'gitlens'-feature
+local cmd = vim.api.nvim_command
+cmd('set encoding=utf-8')         -- set default encoding standard
+cmd('set hidden')                 -- allow switching buffers without saving them
+cmd('set relativenumber')         -- set relative line numbering
+cmd('set termguicolors')          -- enable 24-bit rgb
+cmd('set tabstop=2')              -- sets tab-length to 2 instead of the default 8
+cmd('set shiftwidth=2')           -- enables auto-indenting to the same degree of tab-lengths
+cmd('set expandtab')              -- converts all tabs to spaces on save
+cmd('set number')                 -- show numbers on side
+cmd('set noswapfile')             -- disable swap-files
+cmd('set nobackup')               -- disable backup files
+cmd('set incsearch')              -- incremental searching
+cmd('set ignorecase')             -- searching ignores case
+cmd('set smartcase')              -- use case-aware search only if search is capitalized
+cmd('set cursorline')             -- highlights current line
+cmd('set scrolloff=8')            -- always pad the top / bottom of the buffer by 8 lines when scrolling
+cmd('set nowrap')                 -- disable line-wrapping
+cmd('set noshowmode')             -- disables messages below the tabline
+cmd('set clipboard+=unnamedplus') -- enables copy / pasting to and from external programs
+cmd('set showtabline=2')          -- always show tab-bar
+cmd('set mouse=a')                -- enable mouse usage
+cmd('set autoindent')             -- auto-indenting
+cmd('colorscheme gruvbox')        -- set colorscheme
+cmd('set bg=dark')                -- set background-color
+cmd('let g:blamer_enabled=1')     -- enable 'gitlens'-feature
+cmd('set list')                   -- enable list characters
+cmd('set showbreak=↪\\ ')         -- character to show before the continuation of a wrapped line
+cmd('set listchars=tab:→\\ ,nbsp:␣,trail:•,extends:⟩,precedes:⟨')
 
--- Enable alpha
-vim.cmd('autocmd BufRead,BufNew,BufEnter * highlight Normal guibg=none')
--- disables auto-comment continuations for all files.
--- Some files have their format-options overwritten by
--- ftplugins; this ensures that they are set correctly
-vim.cmd('autocmd BufRead,BufNew,BufEnter *.py* set formatoptions-=cro shiftwidth=4 tabstop=4 expandtab')
--- run xrdb on changes to xresources
-vim.cmd('autocmd BufWritePost ~/.xresources !xrdb -DPYWAL_="<$HOME/.cache/wal/colors.Xresources>" -merge ~/.xresources')
--- recompile dwm config on write
-vim.cmd('autocmd BufWritePost ~/.config/dwm/config.h !cd ~/.config/dwm/; sudo make install')
--- recompile st config on write
-vim.cmd('autocmd BufWritePost ~/.config/st/config.h !cd ~/.config/st/; sudo make install')
--- recompile + restart dwmblocks config on write
--- (yes, this is a very ugly way of doing this)
-vim.cmd('autocmd BufWritePost ~/.config/dwmblocks/config.h !cd ~/.config/dwmblocks/; sudo make install; kill $(pidof -s dwmblocks) >/dev/null; dwmblocks &')
--- recompile dmenu config on write
-vim.cmd('autocmd BufWritePost ~/.config/dmenu/config.h !cd ~/.config/dmenu/; sudo make install')
--- recompile slock config on write
-vim.cmd('autocmd BufWritePost ~/.config/slock/config.h !cd ~/.config/slock/; sudo make install')
+-- enable alpha
+cmd('autocmd BufRead,BufNew,BufEnter * highlight Normal guibg=none')
+
 -- set filetype to bash for all .sh files
-vim.cmd('autocmd BufRead,BufNew,BufEnter *.sh* set syntax=bash')
+cmd('autocmd BufRead,BufNew,BufEnter *.sh* set syntax=bash')
+
 -- disable error highlighting in json files
-vim.cmd('autocmd BufRead,BufNew,BufEnter *.json* hi Error NONE')
+cmd('autocmd BufRead,BufNew,BufEnter *.json* hi Error NONE')
+
+-- disables auto-comment continuations for .py files (some filetypers have their format-options overwritten by ftplugins)
+cmd('autocmd BufRead,BufNew,BufEnter *.py* set formatoptions-=cro shiftwidth=4 tabstop=4 expandtab')
+
+-- run a configurable autocmd from the parent folder of 'file' when 'file' is written to
+local function action_on_write(file, command)
+  if not command then
+    local parent_folder = file:match(".*/")
+    command = '!cd ' .. parent_folder .. '; sudo make install'
+  end
+  cmd('autocmd BufWritePost ' .. file .. ' ' .. command)
+end
+
+-- recompile suckless / xresource configs on write
+action_on_write('~/.config/dwm/config.h')
+action_on_write('~/.config/st/config.h')
+action_on_write('~/.config/dmenu/config.h')
+action_on_write('~/.config/slock/config.h')
+action_on_write('~/.config/dwmblocks/config.h', '!cd ~/.config/dwmblocks/; sudo make install; kill $(pidof -s dwmblocks) >/dev/null; dwmblocks &')
+action_on_write('~/.xresources', 'autocmd BufWritePost ~/.xresources !xrdb -DPYWAL_="<$HOME/.cache/wal/colors.Xresources>" -merge ~/.xresources')
 
