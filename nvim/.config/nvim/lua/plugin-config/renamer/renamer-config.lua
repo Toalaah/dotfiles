7fd8bde79b6eabbb62a3f5dcecfce0465d3076a0
Samuel Kunst <samuel.kunst@gmail.com>
2022-04-21 21:47:05 +0200

    nvim(fix): fix offset bug with lsp-rename plugin
    (likely) caused by upgrading to neovim 0.7
    
    Change taken from thread in renamer.nvim plugin disussing the issue

--- nvim/.config/nvim/lua/plugin-config/renamer/renamer-config.lua
+++ nvim/.config/nvim/lua/plugin-config/renamer/renamer-config.lua
@@ -13,3 +13,15 @@ require('renamer').setup({
   with_popup = true,
   handler = nil,
 })
+
+
+local r = require"renamer"
+r.setup {}
+r._apply_workspace_edit = function(resp)
+  local params = vim.lsp.util.make_position_params()
+  local results_lsp, _ = vim.lsp.buf_request_sync(0,
+      require"renamer.constants".strings.lsp_req_rename, params)
+  local client_id = results_lsp and next(results_lsp) or nil
+  local client = vim.lsp.get_client_by_id(client_id)
+  require"vim.lsp.util".apply_workspace_edit(resp, client.offset_encoding)
+end
