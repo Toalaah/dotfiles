62d712196fb4977697eb01afb499b7aef442b151
Samuel Kunst <samuel.kunst@gmail.com>
2022-08-01 09:15:33 +0200

    nvim: auto-compile plugin file on write

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,9 +1,23 @@
-local _env = vim.api.nvim_create_augroup("__env", {clear=true})
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = ".env*",
-  group = _env,
-  callback = function(args)
-    vim.diagnostic.disable(args.buf)
-    vim.bo.filetype="sh"
-  end
-})
+---@param event string
+---@param pattern string
+---@param group string
+---@param callback function
+local au_cmd = function(event, pattern, group, callback)
+  vim.api.nvim_create_autocmd(event, {
+    pattern = pattern,
+    group = vim.api.nvim_create_augroup(group, { clear = true }),
+    callback = callback,
+  })
+end
+
+au_cmd('BufEnter', '*.env*', '__env', function(args)
+  vim.diagnostic.disable(args.buf)
+  vim.bo.filetype = 'sh'
+end)
+
+au_cmd('BufWritePost', '*/plugins/init.lua', '__packer', function(_)
+  vim.cmd([[
+      luafile %
+      PackerCompile
+    ]])
+end)
