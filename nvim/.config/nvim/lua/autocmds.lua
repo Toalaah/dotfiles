662f447ce7ce14adfa47d054916aea062921ee65
Samuel Kunst <samuel.kunst@gmail.com>
2022-08-04 09:31:01 +0200

    nvim: clean up autocommands

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,8 +1,8 @@
 ---@param event string
 ---@param pattern string
 ---@param group string
----@param callback function
-local au_cmd = function(event, pattern, group, callback)
+---@param callback function|string
+local function au_cmd(event, pattern, group, callback)
   vim.api.nvim_create_autocmd(event, {
     pattern = pattern,
     group = vim.api.nvim_create_augroup(group, { clear = true }),
@@ -10,14 +10,52 @@ local au_cmd = function(event, pattern, group, callback)
   })
 end
 
+-- Set syntax of .env files to sh
 au_cmd('BufEnter', '*.env*', '__env', function(args)
   vim.diagnostic.disable(args.buf)
   vim.bo.filetype = 'sh'
 end)
 
+-- Auto-sync / compile packer on changes to plugin file
 au_cmd('BufWritePost', '*/plugins/init.lua', '__packer', function(_)
-  vim.cmd([[
-      luafile %
-      PackerCompile
-    ]])
+  require('lua.util').reload_module()
+  require('packer').sync()
 end)
+
+-- Update xresources on write
+au_cmd(
+  'BufWritePost',
+  '~/.xresources',
+  '__xresources',
+  '!xrdb -DPYWAL_="<$HOME/.cache/wal/colors.Xresources>" -merge ~/.xresources'
+)
+
+-- Automatically compile suckless programs on write
+-- Need to clear the autogroup first to avoid stacking commands
+local group = '__suckless'
+vim.api.nvim_create_augroup(group, { clear = true })
+for _, v in ipairs({
+  '~/.config/dwm/config.h',
+  '~/.config/st/config.h',
+  '~/.config/dmenu/config.h',
+  '~/.config/slock/config.h',
+}) do
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = v,
+    group = vim.api.nvim_create_augroup(group, {
+      clear = false,
+    }),
+    callback = string.format('!cd %s; sudo make install', v),
+  })
+end
+
+-- Dwmblocks requires additional process termination on top of simply running 'make'
+au_cmd(
+  'BufWritePost',
+  '~/.config/dwmblocks/config.h',
+  '__dwmblocks',
+  '!cd ~/.config/dwmblocks/; sudo make install; kill $(pidof -s dwmblocks) >/dev/null; dwmblocks &'
+)
+
+-- disables auto-comment continuations for .py files (some filetypes have their format-options overwritten by ftplugins)
+vim.cmd([[autocmd BufRead,BufNew,BufEnter *.py* set formatoptions-=cro shiftwidth=4 tabstop=4 expandtab]])
