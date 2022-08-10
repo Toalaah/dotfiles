c826bd30aef747b915609c512856b9130068fa61
Samuel Kunst <samuel.kunst@gmail.com>
2022-08-10 09:30:06 +0200

    nvim: extract impatient setup to separate file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,5 @@
--- check for cached lua modules
-_G.__luacache_config = {
-  chunks = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_chunks',
-  },
-  modpaths = {
-    enable = true,
-    path = vim.fn.stdpath('cache') .. '/luacache_modpaths',
-  },
-}
-pcall(require, 'impatient')
+-- check for cached lua modules via impatient.nvim
+require('cache')
 -- general preferences and options
 require('preferences')
 -- load and initialize plugins
