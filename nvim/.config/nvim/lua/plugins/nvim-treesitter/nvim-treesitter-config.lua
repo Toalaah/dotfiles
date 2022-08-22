c9a19a70d1b4886a3f0b3163e2eccba3fbb3cdfb
Samuel Kunst <samuel.kunst@gmail.com>
2022-08-22 09:25:26 +0200

    nvim: clean up treesitter config

--- nvim/.config/nvim/lua/plugins/nvim-treesitter/nvim-treesitter-config.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter/nvim-treesitter-config.lua
@@ -1,27 +1,6 @@
 require('nvim-treesitter.configs').setup({
+  ensure_installed = { "lua" },
   highlight = {
     enable = true,
-  },
-  textobjects = {
-    move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
   },
 })
