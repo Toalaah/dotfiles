4fed28165d24e3b88c792af603f6e00cbafdfb81
Samuel Kunst <samuel.kunst@gmail.com>
2022-11-09 09:20:44 +0100

    nvim: update notification style

--- nvim/.config/nvim/lua/plugins/nvim-notify/nvim-notify-config.lua
+++ nvim/.config/nvim/lua/plugins/nvim-notify/nvim-notify-config.lua
@@ -3,8 +3,10 @@ local opts = {
   fps = 60,
   max_width = 80,
   max_height = 15,
-  timeout = 2500,
-  background_color = '#000000',
+  timeout = 1500,
+  top_down = true,
+  render = 'minimal',
+  background_colour = '#00FF00',
 }
 local notify = require('notify')
 notify.setup(opts)
