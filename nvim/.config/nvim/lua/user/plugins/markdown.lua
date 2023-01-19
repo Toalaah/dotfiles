7f27f12f2491d43b72e98ec8a092919def882a9e
Samuel Kunst <samuel@kunst.me>
2023-01-19 09:40:27 +0100

    nvim: add markdown image paste plugin

--- nvim/.config/nvim/lua/user/plugins/markdown.lua
+++ nvim/.config/nvim/lua/user/plugins/markdown.lua
@@ -1,6 +1,12 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  ft = 'markdown',
-  build = function() vim.fn['mkdp#util#install']() end,
-  config = function() vim.g.mkdp_auto_close = false end,
+  {
+    'iamcco/markdown-preview.nvim',
+    ft = 'markdown',
+    build = function() vim.fn['mkdp#util#install']() end,
+    config = function() vim.g.mkdp_auto_close = false end,
+  },
+  {
+    'ekickx/clipboard-image.nvim',
+    ft = 'markdown',
+  },
 }
