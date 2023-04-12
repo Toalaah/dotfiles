b58160a98cd3f8884c8bae7df4a9655b80dfb456
Samuel Kunst <samuel@kunst.me>
2023-04-12 21:52:10 +0200

    bump eww overlay version

--- overlays/eww/default.nix
+++ overlays/eww/default.nix
@@ -1,15 +1,16 @@
-{prev}:
+{prev, gtk-layer-shell}:
 prev.eww.overrideAttrs (old: rec {
-  version = "6a64a286291456984011c4fb160def106ee55dff";
+  buildInputs = old.buildInputs ++ [gtk-layer-shell];
+  version = "e76206817de1cb86ec431dcff7d4b04c8b7d36fc";
   src = prev.fetchFromGitHub {
     owner = "elkowar";
     repo = "eww";
     rev = version;
-    sha256 = "sha256-IliWerC9qk4MxYNRoe/8uuQZkqjPJPsph46fmVBvo9U=";
+    sha256 = "sha256-7zN4FWGFpHo3SCe/c5ximxbQyikY8nUBgp+QHUkGsFQ=";
   };
   cargoDeps = old.cargoDeps.overrideAttrs (_old': {
     inherit src;
-    outputHash = "sha256-K/vTAQDu8Fe/Lwr28P/sHjHVyh/5hZh7pAp2nCKfWps=";
+    outputHash = "sha256-ereoJ2SdnMeX/s1DPklBHxUsFmD7RM9Mt/9DG2HQk5E=";
     patches = [];
   });
 
