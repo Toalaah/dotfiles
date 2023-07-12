a3d5b43f9f426900bf87e3544e4cee2453cd9de1
Samuel Kunst <samuel@kunst.me>
2023-07-12 09:00:58 +0200

    add pavucontrol to graphical profile

--- nixos/profiles/graphical/default.nix
+++ nixos/profiles/graphical/default.nix
@@ -1,8 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   sound.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
   };
+  environment.systemPackages = [pkgs.pavucontrol];
 }
