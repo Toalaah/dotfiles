5847fa88103f8b24914afdd24ffb0ffdddc26846
Samuel Kunst <samuel@kunst.me>
2023-07-20 12:49:53 +0200

    eww: handle corner-case when charge percent not found

--- home/modules/wm/eww/configs/sovl/widgets/battery/get-battery-level.sh
+++ home/modules/wm/eww/configs/sovl/widgets/battery/get-battery-level.sh
@@ -7,7 +7,7 @@ info=$(acpi | jc --acpi | jq '.[0]')
 # filter power supplies (which always give charge of 0). This only breaks when the
 # laptop battery is empty, but this would also imply that the laptop (and thus
 # this script) is dead
-if [ $(jq '.charge_percent != 0' <<< $info) == "true" ]; then
+if [ $(jq '.charge_percent > 0 and .charge_percent != null' <<< $info) == "true" ]; then
   echo $info
 else
   echo 'null'
