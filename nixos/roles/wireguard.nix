98eeaacc9cc63609ffcd19be10289ef9af1b9733
Samuel Kunst <samuel@kunst.me>
2023-10-24 18:08:40 +0200

    fix wireguard aliases with sudo

--- nixos/roles/wireguard.nix
+++ nixos/roles/wireguard.nix
@@ -10,16 +10,22 @@
     "d /etc/wireguard 770 root root"
   ];
 
+  # alias command to full path in order to use commands w/ sudo more conveniently
+  environment.shellAliases."wg-quick" = "${pkgs.wireguard-tools}/bin/wg-quick";
+  environment.shellAliases."wg" = "${pkgs.wireguard-tools}/bin/wg";
+  # https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+  environment.shellAliases."sudo" = "sudo ";
+
   security.sudo.extraRules = [
     {
       users = [config.attributes.primaryUser.name];
       commands = [
         {
-          command = "${pkgs.wireguard-tools}/bin/wg-quick";
+          command = "${pkgs.wireguard-tools}/bin/wg-quick *";
           options = ["NOPASSWD"];
         }
         {
-          command = "${pkgs.wireguard-tools}/bin/wg";
+          command = "${pkgs.wireguard-tools}/bin/wg *";
           options = ["NOPASSWD"];
         }
       ];
